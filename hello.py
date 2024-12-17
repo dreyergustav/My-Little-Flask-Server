c1ab333de9ff1a5ec8ec834b2339391b6ee7cdb2
Michael Dreyer Kollemorten <michael@Michaels-MacBook-Pro.local>
2024-12-17 10:35:48 +0100

    added logging to file

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
+import logging
+import os
+
 from flask import Flask
+
+logging.basicConfig(filename=os.environ.get('FLASK_LOG_PATH', '/tmp/flask.log') ,level=logging.INFO)
 
 app = Flask(__name__)
 
